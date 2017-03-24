f794bb1ba683d44402c294babaeedf677b09d911
Bin Zailani <binzailani3136@gmail.com>
2017-03-25 00:41:59 +0800

    fix: initdb enhancement for Unix and Windows platforms

--- scripts/initdb.js
+++ scripts/initdb.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
 var fixtures = require('pow-mongoose-fixtures')
+var path = require('path')
 var db = require('../db')
 
 /// Connect to mongodb and init the db
@@ -12,8 +13,14 @@ mongoose.connection.on('error', function () {
 
 mongoose.connection.once('open', function () {
     process.stdout.write('Loading fixtures ... ')
-    fixtures.load(__dirname + '/fixtures.js', mongoose, function(err) {
+
+    var fixture_path = path.resolve(__dirname + '/fixtures.js');
+    if (process.platform == "win32"){
+        fixture_path = fixture_path.substr(3);
+    }
+
+    fixtures.load(fixture_path, mongoose, function(err) {
         process.stdout.write('done.\n'.green)
         process.exit()
-    })
+    });
 })
